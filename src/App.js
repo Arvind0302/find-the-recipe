a65629499baa6c5fefc5973c2406cae47d107e91
Arvind Yadav <arvind02dk@gmail.com>
2022-02-24 11:51:37 +0530

    loading bar added

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import React from "react";
 import { useState } from "react";
 import axios from "axios";
 import RecipeCard from "./components/RecipeCard";
+import LoadingBar from 'react-top-loading-bar'
 
 function App() {
   const [query, setquery] = useState("");
   const [recipes, setrecipes] = useState([]);
+  const [progress, setProgress] = useState(0)
   const id = "e57e17f7";
   const key = "1c87be1ee870b9dfe00a0be3741640ad";
   var url = `https://api.edamam.com/search?q=${query}&app_id=${id}&app_key=${key}`;
 
   async function getrecipe() {
+    setProgress(10);
     var results = await axios.get(url);
+    setProgress(50);
     setrecipes(results.data.hits);
+    setProgress(100);
     // console.log(results.data.hits);
   }
 
@@ -24,6 +29,12 @@ function App() {
 
   return (
     <div>
+    <div>
+      <LoadingBar
+        height={3}
+        color='#00ff00'
+        progress={progress} 
+      />
       <div className=" top d-flex flex-column justify-content-center align-items-center">
         <h1>FIND THE RECIPE <i className="fas fa-utensils"></i></h1>
         <form className="d-flex" onSubmit={OnSubmit}>
@@ -50,6 +61,7 @@ function App() {
           })}
         </div>
       </div>
+    </div>
     </div>
   );
 }
